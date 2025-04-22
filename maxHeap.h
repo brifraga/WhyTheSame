3369a77bbb377a87194c64a8cacf8b1d49495b73
Tomtron623 <60526206+Tomtron623@users.noreply.github.com>
2025-04-22 19:45:50 -0400

    FINAL VERSION, CLEANED CODE for real this time

--- maxHeap.h
+++ maxHeap.h
@@ -69,8 +69,6 @@ public:
     pair<int, string> getMax(string genre, vector<string> filter = {});
     //Gets all wordd sorted by frequency within a genre, has optional string vector with words to filter
     vector<pair<int, string>> getAll(string genre, vector<string> filter = {}); // Get all words sorted by frequency
-    //void clear(); // Clears heap and map
-    vector<pair<string, int>> getTopWordsByGenre(string genre, int topN, vector<string> filter = {});
 
 
 };
